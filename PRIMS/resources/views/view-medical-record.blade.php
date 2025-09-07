498215a9e5829d4780f9a900e5525026ee9bdb74
smcatingub <smcatingub@student.apc.edu.ph>
2025-09-07 14:31:46 +0800

    updated back button

--- PRIMS/resources/views/view-medical-record.blade.php
+++ PRIMS/resources/views/view-medical-record.blade.php
@@ -1,12 +1,18 @@
 <x-app-layout>
     <div class="py-3">
         <div class="max-w-[90rem] mx-auto sm:px-6 lg:px-8">
+            <div class="flex items-center gap-1">
             <x-prims-sub-header>
-                <a href="{{ url()->previous() }}" class="text-prims-yellow-1">
-                    [ < back ]
+                <a href="{{ url()->previous() }}" class="pr-3">
+                    <
                 </a>
-                / Saved Medical Record
             </x-prims-sub-header>
+            <div class="flex-1">
+                <x-prims-sub-header>
+                    Saved Medical Record
+                </x-prims-sub-header>
+            </div>
+        </div>
             <livewire:view-medical-record :record="$record" />
         </div>
     </div>
